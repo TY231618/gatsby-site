7cd1129ad0392444fb5b4cf106249629ba0275d0
Tony Young <anthony.c.young18@gmail.com>
2021-06-22 23:39:08 +0100

    added additional pages and some styling

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return(
+    <Layout pageTitle="About Page">
+      <p>This is the about page, this is all about me</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
